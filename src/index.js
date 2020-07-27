59078fefa668a58c8c3e3f5976bbe6753480f76b
Laksh Dhamija <laksh.dhamija@gmail.com>
2020-07-27 08:10:35 +0530

    Add action change logger

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 
 import './index.css';
 import App from './components/App';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer);
+const logger = ({dispatch, getState}) => (next) => (action) => {
+    console.log('ACTION_TYPE =', action.type);
+    next(action);
+}
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 ReactDOM.render(<App store={store}/>, document.getElementById('root'));
 
